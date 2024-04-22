e56bf7b5599ac70e207b7c49e7d87e40f277af47
12non <161165908+12non@users.noreply.github.com>
2024-04-22 12:43:38 +0200

    Add files via upload

---
+++ draw_pyramide.py
@@ -0,0 +1,31 @@
+
+
+def draw_pyramide():
+    # Display message
+    print("Questo è una Piramide... \n")
+
+    # Draw the Piramide
+    print(" " *44, "-" * 2)
+    print(" " *42, "-" * 6)
+    print(" " *40, "-" * 10) 
+    print(" " *38, "-" * 14) 
+    print(" " *36, "-" * 18) 
+    print(" " *34, "-" * 22)  
+
+    
+    print(" " *32, "-" * 26)  
+    print(" " *30, "-" * 30)  
+   
+    print(" " *28, "-" * 34)
+    print(" " *26, "-" * 38)
+    print(" " *24, "-" * 42)
+    print(" " *22, "-" * 46)
+    print(" " *20, "-" * 50)
+
+    print(" " *18, "-" * 54)
+
+    # Display message
+    print("\n Piramide drawing completed!")
+
+# Call the function to draw the pyramide - draw_pyramide()
+draw_pyramide()
